0427f5a3a85334bacddec59e337cd224fbe8fd2c
JoaoGabrielGuedes <jgabriel350@gmail.com>
2025-11-12 22:18:42 -0300

    Conditional rendering and data binding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,35 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc'
+
 export default class HelloWorld extends LightningElement {
-        greeting = 'World';
-        changeHandler(event) {
-        this.greeting = event.target.value;
-        }
+  //@track adress
+
+  fullname = '  Zero to Hero'
+  title = 'LWC'
+  changeHandler(event) {
+  this.title = event.target.value
+  }
+
+  address={
+    city: 'New York',
+    state: 'NY',
+    country: 'USA',
+    zip: '10001'
+  }
+  
+  trackHandler(event){    
+    //this.address.city = event.target.value
+    this.address = {...this.address, "city": event.target.value} //segunda forma de fazer sem o track
+  }
+
+  users = ["John", "Jane", "Jack", "Jill"]
+  num1 = 10
+  num2 = 20
+  
+  get firstUser(){
+    return this.users[0]
+  } 
+
+  get multiply(){
+    return this.num1 * this.num2
+  }
 }
